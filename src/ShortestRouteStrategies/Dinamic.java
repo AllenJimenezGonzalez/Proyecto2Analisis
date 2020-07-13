be2a8884d84b247681a5d78c5bc1265705cb5acf
andres <andrestati17@gmail.com>
2020-07-13 15:03:06 -0600

    cambios dijkstra, contadores

--- src/ShortestRouteStrategies/Dinamic.java
+++ src/ShortestRouteStrategies/Dinamic.java
@@ -15,6 +15,10 @@ import Graph.Vertex;
 public class Dinamic {
     static int V = 0;
 
+    public int memory = 0;
+    public int instructions = 0;
+
+    
     public static void setV(int V) {
         Dinamic.V = V;
     }
@@ -47,24 +51,42 @@ public class Dinamic {
         return graph;
     }
 
-    public void Dijkstra(int graph[][], int src) {
+        public void Dijkstra(int graph[][], int src) {
+        instructions++;
+        memory+=32*V;
         int dist[] = new int[V];
 
+        instructions++;
+        memory+=1*V;
         Boolean sptSet[] = new Boolean[V];
 
+        instructions+=3;
+        memory+=32;
         for (int i = 0; i < V; i++) {
+            instructions++;
             dist[i] = Integer.MAX_VALUE;
+            instructions++;
             sptSet[i] = false;
         }
+        
+        instructions++;
         dist[src - 1] = 0;
 
+        instructions+=3;
+        memory+=32;
         for (int count = 0; count < V - 1; count++) {
 
+            instructions++;
+            memory+=32;
             int u = minDistance(dist, sptSet);
 
+            instructions++;
             sptSet[u] = true;
 
+            instructions+=3;
+            memory+=32;
             for (int v = 0; v < V; v++) {
+                instructions+=5;
                 if (!sptSet[v] && graph[u][v] != 0
                         && dist[u] != Integer.MAX_VALUE && dist[u] + graph[u][v] < dist[v]) {
                     dist[v] = dist[u] + graph[u][v];
@@ -76,6 +98,8 @@ public class Dinamic {
 
     public void printSolution(int dist[], int n) {
         System.out.println("Vertex   Distance from Source");
+        instructions+=3;
+        memory+=32;
         for (int i = 0; i < V; i++) {
             System.out.println(i + 1 + " tt " + dist[i]);
         }
